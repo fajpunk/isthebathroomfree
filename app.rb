bd8474b0135ac66c3f0d8714ebfff7bd408d7660
Dan Fuchs <fuchsd@gmail.com>
2014-06-07 11:20:43 -0500

    I think the spark part works...

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'redis'
+
+DOOR_KEY = 'bathroom.door'
+set :bind, '0.0.0.0'
+set :redis, Redis.new
 
 get '/' do
-  'Hello world!'
+  val = settings.redis.get(DOOR_KEY)
+  "The door is: #{val}"
+end
+
+get '/api/door_opened' do
+  settings.redis.set(DOOR_KEY, 'open')
+end
+
+get '/api/door_closed' do
+  settings.redis.set(DOOR_KEY, 'closed')
 end
--- spark/reed-switch-test.ino
+++ spark/reed-switch-test.ino
@@ -1,37 +1,58 @@
 int door_sensor = D0;
 int indicator = D4;
 int door_reading = 0;
-volatile unsigned long last_interrupt_time = 0;
-volatile int door_flag= 0;
+int new_door_reading;
+byte api[] = {192, 168, 0, 110};
 
+TCPClient client;
 
 void setup() {
-    pinMode(door_sensor, INPUT_PULLDOWN);
-    pinMode(indicator, OUTPUT);
-    digitalWrite(indicator, LOW);
-    
-    attachInterrupt(door_sensor, door_change, CHANGE);
+  Serial.begin(9600);
+
+  pinMode(door_sensor, INPUT_PULLDOWN);
+  pinMode(indicator, OUTPUT);
+  digitalWrite(indicator, LOW);
 }
 
 
 void loop() {
-    if (door_flag) {
-        door_reading = digitalRead(door_sensor);
-        digitalWrite(indicator, door_reading);  
-        if (door_reading == HIGH) {
-            Spark.publish("door-closed", NULL, 60, PRIVATE);
-        } else {
-            Spark.publish("door-opened", NULL, 60, PRIVATE);
-        }
-        door_flag = 0;
+  new_door_reading = digitalRead(door_sensor);
+  if (door_reading != new_door_reading) {
+    door_reading = new_door_reading;
+    digitalWrite(indicator, door_reading);  
+    if (door_reading == HIGH) {
+      send_event("door_closed");
+    } else {
+      send_event("door_opened");
     }
+  }
 }
 
+void send_event(String event) {
+  Serial.println("Sending ");
 
-void door_change() {
-    unsigned long interrupt_time = millis();
-    if (interrupt_time - last_interrupt_time > 1000) {
-        door_flag = 1;
-        last_interrupt_time = interrupt_time;
+  if (client.connect(api, 5000)) {
+    Serial.println(event);
+    Serial.println("Connected to api");
+
+    client.print("GET /api/");
+    client.print(event);
+    client.println(" HTTP/1.1");
+    client.println("User-Agent: sparkcore");
+    client.println("Host: 192.168.0.110:5000");
+    client.println("Accept: */*");
+    client.println("Connection: close");
+    client.println();
+    
+    // Wait for the response
+    while(true) {
+      if (client.available() != 0) {
+        break;
+      }
     }
+    client.stop();
+    Serial.println("Closed connection");
+  } else {
+    Serial.println("Couldn't connect to api");
+  }
 }
